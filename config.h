4098a614bce467b6e8a6c98da051308b11992e94
Sofia Fernandez <sofia.fernandezlozano@mail.mcgill.ca>
2022-04-04 15:25:24 -0400

    Notion rule

--- config.h
+++ config.h
@@ -46,7 +46,7 @@ static const char *const autostart[]    = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "ﬧ", "﫦", "", "", "ﳲ", "", "爵" };
+static const char *tags[]       = { "", "", "ﬧ", "﫦", "", "", "ﳲ", "", "爵" };
 static const char *tagsalt[]    = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -60,6 +60,7 @@ static const Rule rules[] = {
 	{ "St",            NULL,     "lf",           1 << 1,    0,          1,           0,        -1 },
 	{ "RStudio",       NULL,     NULL,           1 << 2,    0,          0,           0,        -1 },
 	{ NULL,            NULL,     "Register",     1 << 2,    1,          0,           0,        -1 },
+	{ "notion-app-enhanced",NULL,NULL,           1 << 3,    0,          0,           0,        -1 },
 	{ "Zotero",        NULL,     NULL,           1 << 3,    0,          0,           0,        -1 },
 	{ "calibre",       NULL,     NULL,           1 << 3,    0,          0,           0,        -1 },
 	{ NULL,   "libreoffice",     NULL,           1 << 3,    0,          0,           0,        -1 },
