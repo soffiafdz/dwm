b3591661a5712d0aab6ae08875d198d025d23021
Sofía Fernández <sofia.fernandezlozano@mail.mcgill.ca>
2026-02-08 20:03:33 -0500

    Icons

--- config.h
+++ config.h
@@ -47,7 +47,7 @@ static const char *const autostart[]    = {
 };
 
 /* tagging */
-static const char *tags[]       = { "", "", "󰿉", "﫦", "", "", "󰑈", "", "󰖟" };
+static const char *tags[]       = { "", "", "󰿉", "", "", "", "󰑈", "", "󰖟" };
 static const char *tagsalt[]    = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
